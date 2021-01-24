7e077f16f6d4c8961c9a732ccd7d82009fe897c5
Konstantin Komovits <konsta.2106@gmail.com>
2021-01-24 15:52:58 +0200

    Test env variables

--- client/src/index.js
+++ client/src/index.js
@@ -13,3 +13,6 @@ ReactDOM.render(
     <Provider store={store}><App /></Provider>,
     document.querySelector('#root')
     )
+
+console.log(process.env.NODE_ENV)
+console.log(process.env.REACT_APP_PUB_KEY)
